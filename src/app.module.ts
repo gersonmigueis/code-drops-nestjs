31410e413f53b3787581821dc460f31230ed0026
Gerson Migueis <gersonmigueis10@gmail.com>
2021-07-18 22:13:19 -0400

    [IPDATE] - Config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://gerson:aCkWNQz5d2SZqwoU@codedrops.cuz25.mongodb.net/test',
+    ),
+    UsersModule,
+  ],
 })
 export class AppModule {}
